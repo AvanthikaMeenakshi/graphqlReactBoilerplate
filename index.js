5b5dd383adeb130817fff3f9581ff5afad3b3d16
Avanthika Meenakshi <avanthika157@gmail.com>
2019-08-06 20:13:02 +0100

    Added queries

--- index.js
+++ index.js
@@ -1,26 +1,61 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
+var mysql = require('mysql');
 
-// Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type User {
+    id: String
+    name: String
+    job_title: String
+    email: String
+  }
   type Query {
-    hello: String
+    getUsers: [User],
+    getUserInfo(id: Int) : User
+  }
+  type Mutation {
+    updateUserInfo(id: Int, name: String, email: String, job_title: String) : Boolean
+    createUser(name: String, email: String, job_title: String) : Boolean
+    deleteUser(id: Int) : Boolean
   }
 `);
 
-// The root provides a resolver function for each API endpoint
+const queryDB = (req, sql, args) => new Promise((resolve, reject) => {
+    req.mysqlDb.query(sql, args, (err, rows) => {
+        if (err)
+            return reject(err);
+        rows.changedRows || rows.affectedRows || rows.insertId ? resolve(true) : resolve(rows);
+    });
+});
+
 var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
+  getUsers: (args, req) => queryDB(req, "select * from users").then(data => data),
+  getUserInfo: (args, req) => queryDB(req, "select * from users where id = ?", [args.id]).then(data => data[0]),
+  updateUserInfo: (args, req) => queryDB(req, "update users SET ? where id = ?", [args, args.id]).then(data => data),
+  createUser: (args, req) => queryDB(req, "insert into users SET ?", args).then(data => data),
+  deleteUser: (args, req) => queryDB(req, "delete from users where id = ?", [args.id]).then(data => data)
 };
 
 var app = express();
+
+app.use((req, res, next) => {
+  req.mysqlDb = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'userapp'
+  });
+  req.mysqlDb.connect();
+  next();
+});
+
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 }));
+
 app.listen(4000);
+
 console.log('Running a GraphQL API server at localhost:4000/graphql');
