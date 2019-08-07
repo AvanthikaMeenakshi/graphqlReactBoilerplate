e17595bbba5e81f110c79b8f42558c91cc7fe691
Avanthika Meenakshi <avanthika157@gmail.com>
2019-08-07 19:24:18 +0100

    Added fixes

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
